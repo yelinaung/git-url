27b86feb5945b677fb7200efc98eb033dd6942df
Ye Lin Aung <me@yelinaung.com>
2015-03-30 17:54:49 +0630

    add test for bitbucket but this will fail

--- git_url_test.go
+++ git_url_test.go
@@ -8,13 +8,13 @@ import (
 var (
 	sshUrl  = "git@github.com:yelinaung/git-url.git"
 	httpUrl = "https://github.com/yelinaung/git-url.git"
+
+	bitbucketSshUrl  = "git@bitbucket.org:myname/myrepo.git"
+	bitbucketHttpUrl = "https://bitbucket.org/myname/myrepo.git"
 )
 
 func TestAreMenNamesRamdonOrNot(t *testing.T) {
-
 	assert := assert.New(t)
-
-	assert.Equal(123, 123, "they should be equal")
 
 	ssh2http := Ssh2Http(sshUrl)
 	assert.Equal(ssh2http, httpUrl, "they should be equanl")
@@ -22,4 +22,9 @@ func TestAreMenNamesRamdonOrNot(t *testing.T) {
 	http2ssh := Http2Ssh(httpUrl)
 	assert.Equal(http2ssh, sshUrl, "they should be equanl")
 
+	bitbucketSsh2Http := Ssh2Http(bitbucketSshUrl)
+	assert.Equal(bitbucketSsh2Http, bitbucketHttpUrl, "they should be equanl")
+
+	bitbucketHttp2Ssh := Http2Ssh(bitbucketHttpUrl)
+	assert.Equal(bitbucketHttp2Ssh, bitbucketSshUrl, "they should be equanl")
 }
