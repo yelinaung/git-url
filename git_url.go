5b4f578672a41dcfb90ea8b70d2c45e777366208
Ye Lin Aung <me@yelinaung.com>
2015-03-30 17:19:38 +0630

    add codes

---
+++ git_url.go
@@ -0,0 +1,27 @@
+package giturl
+
+import (
+	"fmt"
+	str "strings"
+)
+
+func Http2SSH(httpUrl string) string {
+
+	userNameRepoPair := str.Split(httpUrl, "https://")[1]
+	tempPair := str.Split(userNameRepoPair, "/")
+	userName := tempPair[1]
+	repoName := str.Split(tempPair[2], ".git")[0]
+
+	return fmt.Sprintf("git@github.com:%s/%s.git", userName, repoName)
+}
+
+func SSH2Http(sshUrl string) string {
+	// fmt.Printf("%s is http ? %t\n", sshUrl, str.Contains(sshUrl, http))
+
+	userNameRepoPair := str.Split(sshUrl, ":")[1]
+	tempPair := str.Split(userNameRepoPair, "/")
+	userName := tempPair[0]
+	repoName := str.Split(tempPair[1], ".git")[0]
+
+	return fmt.Sprintf("https://github.com/%s/%s.git", userName, repoName)
+}
---
+++ git_url_test.go
@@ -0,0 +1,25 @@
+package giturl
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var (
+	sshUrl  = "git@github.com:yelinaung/git-url.git"
+	httpUrl = "https://github.com/yelinaung/git-url.git"
+)
+
+func TestAreMenNamesRamdonOrNot(t *testing.T) {
+
+	assert := assert.New(t)
+
+	assert.Equal(123, 123, "they should be equal")
+
+	ssh2http := SSH2Http(sshUrl)
+	assert.Equal(ssh2http, httpUrl, "they should be equanl")
+
+	http2ssh := Http2SSH(httpUrl)
+	assert.Equal(http2ssh, sshUrl, "they should be equanl")
+
+}
